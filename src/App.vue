ae97ba3e4aafdc57dc28affe4065d696a4a5a520
Sokolvek <vadim200608@yandex.ru>
2024-09-22 22:50:43 +0400

    feat: started work on navbar

--- src/App.vue
+++ src/App.vue
@@ -1,25 +1,14 @@
 <script setup lang="ts">
-import { Alert } from 'vue-tg';
-
+// import { Alert } from 'vue-tg';
+import Navbar from "./components/layout/Navbar.vue";
 </script>
 
 <template>
   <div>
-    <Alert message="hello" />
+    <Navbar />
   </div>
 </template>
 
 <style scoped>
-.logo {
-  height: 6em;
-  padding: 1.5em;
-  will-change: filter;
-  transition: filter 300ms;
-}
-.logo:hover {
-  filter: drop-shadow(0 0 2em #646cffaa);
-}
-.logo.vue:hover {
-  filter: drop-shadow(0 0 2em #42b883aa);
-}
+
 </style>
---
+++ src/components/layout/Navbar.vue
@@ -0,0 +1,58 @@
+<template>
+    <nav>
+        <AchievementIcon fill="green" />
+        <ul>
+            <li v-for="navItem in navItems" :key="navItem.text">
+                <img :src="navItem.icon" alt="">
+                <p>{{ navItem.text }}</p>
+            </li>
+        </ul>
+    </nav>
+
+</template>
+
+<script lang="ts" setup>
+import AchievementIcon from '../icons/AchievementIcon.vue';
+
+type navItemsd = {
+    icon:string,
+    text:string,
+    href:string,
+    active:boolean
+}
+
+const navItems: navItemsd[] = [
+    {
+        icon:"/daily-icon.svg",
+        text:"Дейлики",
+        href:"",
+        active: true
+    },
+    {
+        icon:"/shop-icon.svg",
+        text:"Магазин",
+        href:"",
+        active: false
+    },
+    {
+        icon:"/leaderboard-icon.svg",
+        text:"Лидерборд",
+        href:"",
+        active: false
+    },
+    {
+        icon:"/achievement-icon.svg",
+        text:"Достижения",
+        href:"",
+        active: false
+    }
+]
+
+</script>
+
+<style>
+
+nav{
+    background: black;
+}
+</style>
